30c45f00b090469911897da5824c30634be20f53
SergioTanakaAtos <sergio.tanaka@atos.net>
2023-04-25 14:27:10 -0300

    agenda

--- contatos/urls.py
+++ contatos/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('contato/<int:contato_id>', views.ver_contato, name='ver_contato' ),
     path('dashbord/', dashboard, name='adicionar' ),
     path('contato/editar/<int:contato_id>', views.editar, name='editar' ),
-
     path('excluir/<int:contato_id>', views.excluir, name='excluir' ),
 ]
